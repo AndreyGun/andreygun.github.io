7519e0d5d0f107814c83f2b28434f9ff7447d7d1
AndreyGun <andreygunkin90@gmail.com>
2019-02-13 17:49:38 +0200

    1

--- script.js
+++ script.js
@@ -193,25 +193,10 @@ jQuery(document).ready(function($) {
       
   // });
 (function() {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
 
-  var width = 320;    // We will scale the photo width to this
-  var height = 0;     // This will be computed based on the input stream
-
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
-
+  var width = 320; 
+  var height = 0;
   var streaming = false;
-
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
-
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startbutton = null;
 
   function startup() {
     video = document.getElementById('camera');
@@ -230,12 +215,8 @@ jQuery(document).ready(function($) {
         audio: false
       },
       function(stream) {
-        if (navigator.mozGetUserMedia) {
-          video.mozSrcObject = stream;
-        } else {
-          var vendorURL = window.URL || window.webkitURL;
-          video.src = vendorURL.createObjectURL(stream);
-        }
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
         video.play();
       },
       function(err) {
@@ -247,13 +228,6 @@ jQuery(document).ready(function($) {
       if (!streaming) {
         height = video.videoHeight / (video.videoWidth/width);
       
-        // Firefox currently has a bug where the height can't be read from
-        // the video, so we will make assumptions if this happens.
-      
-        if (isNaN(height)) {
-          height = width / (4/3);
-        }
-      
         video.setAttribute('width', width);
         video.setAttribute('height', height);
         canvas.setAttribute('width', width);
@@ -263,16 +237,14 @@ jQuery(document).ready(function($) {
     }, false);
 
     startbutton.addEventListener('click', function(ev){
-     $(".camera-text").text("camera work and photo" );
+     $(".camera-text").text("camera work and photo111" );
       takepicture();
-      ev.preventDefault();
+      //ev.preventDefault();
     }, false);
     
     clearphoto();
   }
 
-  // Fill the photo with an indication that none has been
-  // captured.
 
   function clearphoto() {
     var context = canvas.getContext('2d');
@@ -282,12 +254,6 @@ jQuery(document).ready(function($) {
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   }
-  
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
 
   function takepicture() {
     var context = canvas.getContext('2d');
@@ -302,9 +268,6 @@ jQuery(document).ready(function($) {
       clearphoto();
     }
   }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
   window.addEventListener('load', startup, false);
 })();
 
