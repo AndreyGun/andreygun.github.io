49ff476c177ff2b8f07d6b006d7d032d60ca1f35
AndreyGun <andreygunkin90@gmail.com>
2019-02-14 11:21:32 +0200

    try new script
    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia

--- script.js
+++ script.js
@@ -132,19 +132,64 @@ jQuery(document).ready(function($) {
       video.setAttribute('muted', '');
       video.setAttribute('playsinline', '');
 
-      var constraints = {
-           audio: false,
-           video: {
-               facingMode: 'user'
-           }
-      }
-      navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-        video.srcObject = stream;
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-      });
+      // var constraints = {
+      //      audio: false,
+      //      video: {
+      //          facingMode: 'user'
+      //      }
+      // }
+      // navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+      //   video.srcObject = stream;
+      //   video.setAttribute('width', width);
+      //   video.setAttribute('height', height);
+      //   //canvas.setAttribute('width', width);
+      //   //canvas.setAttribute('height', height);
+      // });
+      // Older browsers might not implement mediaDevices at all, so we set an empty object first
+        if (navigator.mediaDevices === undefined) {
+          navigator.mediaDevices = {};
+        }
+
+        // Some browsers partially implement mediaDevices. We can't just assign an object
+        // with getUserMedia as it would overwrite existing properties.
+        // Here, we will just add the getUserMedia property if it's missing.
+        if (navigator.mediaDevices.getUserMedia === undefined) {
+          navigator.mediaDevices.getUserMedia = function(constraints) {
+
+            // First get ahold of the legacy getUserMedia, if present
+            var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+            // Some browsers just don't implement it - return a rejected promise with an error
+            // to keep a consistent interface
+            if (!getUserMedia) {
+              return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+            }
+
+            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+            return new Promise(function(resolve, reject) {
+              getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+          }
+        }
+
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(function(stream) {
+          var video = document.querySelector('camera');
+          // Older browsers may not have srcObject
+          if ("srcObject" in video) {
+            video.srcObject = stream;
+          } else {
+            // Avoid using this in new browsers, as it is going away.
+            video.src = window.URL.createObjectURL(stream);
+          }
+          video.onloadedmetadata = function(e) {
+            video.play();
+          };
+        })
+        .catch(function(err) {
+          console.log(err.name + ": " + err.message);
+        });
+
     }
       
 
@@ -176,8 +221,8 @@ jQuery(document).ready(function($) {
       });
       var context = canvas.getContext('2d');
       if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
+        canvas.width = canvasW;
+        canvas.height = canvasH;
         context.drawImage(video, 0, 0, canvasW, canvasH);
 
         $("#canvas").addClass("is-visible");
